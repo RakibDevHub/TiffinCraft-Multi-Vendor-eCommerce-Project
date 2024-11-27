1a2e27c0ffb037a62f0649fbc9fd9f5aeeec9c9b
Rakib <rakib.210298@gmail.com>
2024-11-27 23:00:34 +0600

    added some stuff

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,3 @@
-// // navigation links toggle start
-
-// const userLoggedIn = true;
-
-// const loggedInLinks = document.querySelectorAll(".logged-in");
-// const loggedOutLinks = document.querySelectorAll(".logged-out");
-
-// function toggleLinks(loggedIn) {
-//   loggedInLinks.forEach((link) => link.classList.toggle("hidden", !loggedIn));
-//   loggedOutLinks.forEach((link) => link.classList.toggle("hidden", loggedIn));
-// }
-
-// toggleLinks(userLoggedIn);
-
-// // navigation links toggle end
-
-// let menu = document.querySelector("#menu-bar");
-// let navbar = document.querySelector(".nav-links");
-
-// // menu.onclick = () => {
-// //   menu.classList.toggle("fa-times");
-// //   console.log("clicked");
-// // };
-
-// menu.addEventListener("click", () => {
-//   menu.classList.toggle("fa-times");
-//   navbar.classList.toggle("active");
-// });
-
 // Toggle navigation links visibility based on login status
 const userLoggedIn = true;
 
