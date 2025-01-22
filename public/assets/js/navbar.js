95987d8f6d99318732eaab58cb1cd5b71e51b183
Rakib <rakib.210298@gmail.com>
2025-01-22 16:00:08 +0600

    fix problem

--- public/assets/js/navbar.js
+++ public/assets/js/navbar.js
@@ -1,15 +1,16 @@
-// Menu button toggle
 function handleMenubar() {
   let menu = document.querySelector("#menu-bar");
   let navbar = document.querySelector(".nav-links");
 
-  menu.addEventListener("click", () => {
-    menu.classList.toggle("fa-times");
-    navbar.classList.toggle("active");
-  });
+  if (menu && navbar) {
+    // Check if both elements exist
+    menu.addEventListener("click", () => {
+      menu.classList.toggle("fa-times");
+      navbar.classList.toggle("active");
+    });
+  }
 }
 
-// Detect current path and call the appropriate function
 const path = window.location.pathname;
 if (path !== "/business") {
   handleMenubar();
